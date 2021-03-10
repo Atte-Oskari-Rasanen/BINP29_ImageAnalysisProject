144f153ff4a933b5fe1482d59b18464c528fd2a2
Atte-Oskari-Rasanen <70641619+Atte-Oskari-Rasanen@users.noreply.github.com>
2021-03-10 14:04:52 +0100

    image analysis gui but applied in bulk
    BulkImage analysis version2, tried a different approach in UploadAction in terms of getting the images in bulk from the directory

--- BulkImageAnalysis_GUI_2.py
+++ BulkImageAnalysis_GUI_2.py
@@ -1,9 +1,4 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Tue Mar  9 17:29:39 2021
-
-@author: atter
-"""
 
 import tkinter as tk
 from tkinter import filedialog
@@ -14,19 +9,31 @@ from skimage.color import rgb2gray
 import matplotlib.pyplot as plt
 from skimage import measure, filters, morphology
 from skimage import measure
-
+import os
 w = tk.Tk()
 w.title("Fluorescent image analyser")
 
 
 def UploadAction(event=None):
-    imagefile = filedialog.askopenfilename()
-    print('Selected:', imagefile)
-    global cells
-    cells = rgb2gray(imread(imagefile))
-    return(cells)
+
+    global ImRows
+    global cells #make the var global for later use
+    ImRows=[]
+    directory = filedialog.askdirectory()
+    filepaths  = [os.path.join(directory, name) for name in os.listdir(directory)]
+    all_images = []
+
+    for path in filepaths:
+        with open(path) as fIn:
+            cells = rgb2gray(imread(os.path.join(directory,fIn))
+
+            # filename=filename.split(" ")[1]
+            fIn=fIn.name
+            ImRows['ImageID']=fIn
 
 def Thresholds():
+    global TH_vals
+    global TH
     TH_vals = {}
     cell_range = np.unique(cells)
     cell_range=np.round(cell_range, 3)
@@ -42,7 +49,7 @@ def Thresholds():
 def create_window():
     w2 = tk.Toplevel(w)
 
-    def ImageStats():
+    def ImageStats_a():
         ImRows={}
         cells_bw=cells>float(entry.get())
         labels = measure.label(cells_bw)
@@ -54,18 +61,39 @@ def create_window():
         lbl_stats.config(text=ImRows)
     entry = tk.Entry(w2)
     lbl_stats = tk.Label(master=w2, text="Stats appear here after you have entered threshold")
-    btn_stats = tk.Button(
+    btn_stats_a = tk.Button(   #btn_stats_a used if chose option a) --- use of own threshold
         master=w2,
-        text="Get stats",
-        command=ImageStats
+        text="Apply own threshold",
+        command=ImageStats_a
     )
+    def ImageStats_b():
+        ImRows={}
+        cells_bw=cells>TH
+        labels = measure.label(cells_bw)
+        cell_count=labels.max()
+        ImRows['Number_of_cells']=cell_count
+        Average_size=labels.mean()
+        ImRows['Average_cell_size']=Average_size
+        ImRows['Threshold_used']=float(entry.get())
+        lbl_stats.config(text=ImRows)
+
+    btn_stats_b = tk.Button(   #btn_stats_a used if chose option b) --- use of otsu's threshold
+        master=w2,
+        text="Apply Otsu's threshold",
+        command=ImageStats_b
+    )
+
+    lbl_choice = tk.Label(master=w2, text="Enter own threshold or select automatically defined one based on Otsu's method (recommended)")
+    
+    lbl_choice.pack()
+    btn_stats_a.pack()
+    btn_stats_b.pack()
     entry.pack()
     lbl_stats.pack()
-    btn_stats.pack()
     w2.mainloop()
 
 
-btn1 = tk.Button(master=w, text='Click here to open a file', command=UploadAction)
+btn1 = tk.Button(master=w, text='Select an image directory', command=UploadAction)
 btn_ready = tk.Button(
     master=w,
     text="Get threshold values",
